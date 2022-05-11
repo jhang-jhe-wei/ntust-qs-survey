6076bdedd7964dd27580beba9d2665648cfa2462
wells <jhang0912407249@gmail.com>
2022-05-11 22:13:09 +0800

    Add new and create in recommenders

--- app/controllers/recommenders_controller.rb
+++ app/controllers/recommenders_controller.rb
@@ -4,5 +4,22 @@ class RecommendersController < ApplicationController
   def index
     @recommenders = Recommender.includes(:institution, :industry, :users).all
   end
+
+  def new
+    @recommender = Recommender.new
+  end
+
+  def create
+    @recommender = Recommender.new(recommender_params)
+    if @recommender.save
+      redirect_to recommenders_url, notice: "推薦者已建立"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def recommender_params
+    params.require(:recommender).permit(:title, :first_name, :last_name, :job_title, :department, :institution_id, :industry_id)
   end
 end
--- app/helpers/recommenders_helper.rb
+++ app/helpers/recommenders_helper.rb
@@ -1,2 +1,10 @@
 module RecommendersHelper
+  def all_industries_options
+    options_for_select(Industry.all.map{ |industry| [industry.name, industry.id] })
+  end
+
+  def all_institutions_options
+    options_for_select(Institution.all.map{ |institution| [institution.name, institution.id] })
+  end
+
 end
