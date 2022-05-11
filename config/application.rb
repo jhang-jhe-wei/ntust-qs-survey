95c6b13539bcb95becc4714e6b3edc43f5e9ef39
wells <jhang0912407249@gmail.com>
2022-05-11 22:10:01 +0800

    Add i18n config

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module QsSurvey
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = "Taipei"
+    config.i18n.default_locale = "zh-TW"
 
     # Configuration for the application, engines, and railties goes here.
     #
