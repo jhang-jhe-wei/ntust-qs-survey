c606bfb5f96bf949511c839259a1b8390bac91da
wells <jhang0912407249@gmail.com>
2022-05-11 22:12:46 +0800

    Add index in recommenders

--- app/controllers/recommenders_controller.rb
+++ app/controllers/recommenders_controller.rb
@@ -2,5 +2,7 @@ class RecommendersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @recommenders = Recommender.includes(:institution, :industry, :users).all
+  end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "recommenders#index"
+  resources :recommenders, only: [:index, :new, :create]
   devise_for :users
-
 end
