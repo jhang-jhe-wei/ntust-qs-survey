8c8d897d1d4741dfc4dfaa009d93bd5ff47ec17d
wells <jhang0912407249@gmail.com>
2022-06-29 21:29:46 +0800

    Change databaseclean config

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,12 +1,14 @@
 begin
-  DatabaseCleaner.strategy = [:truncation, except: %w[ntust_departments countries institutions industries]]
-
   Before do |_|
     DatabaseCleaner.strategy = [:truncation, except: %w[ntust_departments countries institutions industries]]
   end
 
   After do |_|
     DatabaseCleaner.clean
+  end
+
+  AfterAll do |_|
+    DatabaseCleaner.clean_with :truncation
   end
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
