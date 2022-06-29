cc32901697f72a6d2e73804b2c70de2a6f0ef482
wells <jhang0912407249@gmail.com>
2022-06-29 22:36:23 +0800

    Add dependent in user with recommenders

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :department, class_name: 'NtustDepartment', foreign_key: 'ntust_department_id'
-  has_many :user_and_recommender_records
-  has_many :recommenders, :through => :user_and_recommender_records
+  has_many :user_and_recommender_records, dependent: :destroy
+  has_many :recommenders, :through => :user_and_recommender_records, dependent: :destroy
 end
