1f94caba7f37bdf44b0d93367aa615692a581d06
wells <jhang0912407249@gmail.com>
2022-06-30 11:56:50 +0800

    Set college id after ntust department save

--- app/models/ntust_department.rb
+++ app/models/ntust_department.rb
@@ -2,4 +2,14 @@ class NtustDepartment < ApplicationRecord
   has_many :users, dependent: :destroy
   has_many :departments, class_name: "NtustDepartment", foreign_key: "college_id"
   belongs_to :college, class_name: "NtustDepartment", optional: true
+  after_save :set_college_id, if: :role_is_college?
+
+  private
+  def role_is_college?
+    self.role == 'college'
+  end
+
+  def set_college_id
+    self.update_column(:college_id, self.id)
+  end
 end
