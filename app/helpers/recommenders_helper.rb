6f067d2d3bfa5bce8ae5099cce99d993be7a3ef1
wells <jhang0912407249@gmail.com>
2022-07-04 19:33:32 +0800

    Fix edit recommender cannot find old value in selects

--- app/helpers/recommenders_helper.rb
+++ app/helpers/recommenders_helper.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 module RecommendersHelper
-  def all_industries_options
-    options_for_select(Industry.all.map { |industry| [industry.name, industry.id] })
+  def all_industries_options(default=0)
+    options_for_select(Industry.all.map { |industry| [industry.name, industry.id] }, default)
   end
 
-  def all_institutions_options
-    options_for_select(Institution.all.map { |institution| [institution.name, institution.id] })
+  def all_institutions_options(default=0)
+    options_for_select(Institution.all.map { |institution| [institution.name, institution.id] }, default)
   end
 
-  def all_countries_options
-    options_for_select(Country.all.map { |country| [country.name, country.id] })
+  def all_countries_options(default=0)
+    options_for_select(Country.all.map { |country| [country.name, country.id] }, default)
   end
 
-  def all_categories_options
+  def all_categories_options(default="學術界")
     options_for_select([
                          %w[學術界 學術界],
                          %w[產業界 產業界]
-                       ])
+                       ], default)
   end
 
   def all_departments_options(default = 0)
