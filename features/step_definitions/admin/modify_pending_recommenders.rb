066a605bfc17ac2c98e8bc6b5df411e725c7bfbb
wells <jhang0912407249@gmail.com>
2022-07-06 00:37:15 +0800

    Add pending recommender in step defininations

--- features/step_definitions/admin/modify_pending_recommenders.rb
+++ features/step_definitions/admin/modify_pending_recommenders.rb
@@ -3,6 +3,7 @@
 Given('there are {int} pending recommenders belong to {string} user') do |quantity, department_name|
   department = NtustDepartment.find_by(name: department_name)
   quantity.times do
-    department.users.first.recommenders.create!(attributes_for(:recommender, category: '產業界', title: nil))
+    recommender = department.users.first.recommenders.create!(attributes_for(:recommender, category: '產業界'))
+    recommender.update_columns(status: 'pending')
   end
 end
