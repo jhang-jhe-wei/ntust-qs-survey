5e7dd1c675581decdd11431915f4447243727c78
wells <jhang0912407249@gmail.com>
2022-07-06 01:51:47 +0800

    Remove watch in esbuild.config.js

--- esbuild.dev.config.js
+++ esbuild.dev.config.js
@@ -1,3 +1,4 @@
+
 const esbuild = require('esbuild');
 const { stimulusPlugin } = require('esbuild-plugin-stimulus');
 const path = require('path');
@@ -5,8 +6,8 @@ const path = require('path');
 esbuild.build({
   entryPoints: ["application.js"],
   bundle: true,
-  minify: true,
   outdir: path.join(process.cwd(), "app/assets/builds"),
   absWorkingDir: path.join(process.cwd(), "app/javascript"),
+  watch: true,
   plugins: [stimulusPlugin()],
 }).catch(() => process.exit(1));
