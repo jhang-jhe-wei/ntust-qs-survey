759b7a79db122e5726792f7884bf7c2bf5bd6493
wells <jhang0912407249@gmail.com>
2022-07-08 20:52:04 +0800

    Change stimulus esbuild plugin to stimulus-rails gem

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import { Application } from 'stimulus';
-import { definitions } from 'stimulus:stimulus/controllers';
 
 import './jquery-setup';
 import 'jquery-ui-bundle';
-
-const app = Application.start();
-app.load(definitions);
+import "./controllers"
--- esbuild.config.js
+++ esbuild.config.js
@@ -7,6 +7,7 @@ esbuild.build({
   bundle: true,
   minify: true,
   outdir: path.join(process.cwd(), "app/assets/builds"),
+  watch: !!process.env.DEV_WATCH,
   absWorkingDir: path.join(process.cwd(), "app/javascript"),
   plugins: [stimulusPlugin()],
 }).catch(() => process.exit(1));
