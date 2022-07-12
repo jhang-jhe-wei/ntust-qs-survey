854e9d760b32292590bf1f2cd30a4fd7a0c9cc73
wells <jhang0912407249@gmail.com>
2022-07-12 20:11:33 +0800

    Add recommenders dependent in institutions

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -3,4 +3,5 @@
 class Institution < ApplicationRecord
   belongs_to :country
   validates :name, presence: true, format: { with: /\A[A-Za-z0-9. ,_-]*\z/, message: '限以英文 (半形) 填入，並勿填寫如É、Ÿ等特殊字元。' }
+  has_many :recommenders, dependent: :destroy
 end
