d8bbcaf6d38bb091f680fa2c6ed2cad2fb336f82
wells <jhang0912407249@gmail.com>
2022-07-12 16:50:22 +0800

    Refactor recommender form

---
+++ app/builders/recommender_form_builder.rb
@@ -0,0 +1,11 @@
+class RecommenderFormBuilder < ActionView::Helpers::FormBuilder
+  def text_with_label_field(attribute, options = {})
+    label(attribute, class: "recommender-form-label") +
+    text_field(attribute, {
+      required: true,
+      class: "recommender-form-input",
+      pattern: "[A-Za-z0-9.-]*",
+      title: "限以英文 (半形) 填入，並勿填寫如É、Ÿ等特殊字元。"
+    }.merge(options))
+  end
+end
