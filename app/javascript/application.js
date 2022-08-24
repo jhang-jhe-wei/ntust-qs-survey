2704e2b2ad19b229e64f10e518498ccbea7aba6f
wells <jhang0912407249@gmail.com>
2022-08-24 20:43:43 +0800

    Refactor: lint application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,15 +4,20 @@ import "@hotwired/turbo-rails"
 import './jquery-setup';
 import 'jquery-ui-bundle';
 import './components/CategorySelector';
+import './components/CompanyAutocomplete';
 import ReactRailsUJS from "react_ujs";
 document.addEventListener('turbo:before-visit', ()=>{
-  ReactRailsUJS.unmountComponents()
+  ReactRailsUJS.unmountComponents();
 })
+
 document.addEventListener('turbo:load', ()=>{
-  ReactRailsUJS.mountComponents()
+  ReactRailsUJS.mountComponents();
+  setup();
+})
+
+function setup(){
   const elements = document.querySelectorAll('input,select,textarea');
   const invalidListener = function(){ this.scrollIntoView(false); };
   for(let i = elements.length; i--;)
     elements[i].addEventListener('invalid', invalidListener);
-})
-
+}
